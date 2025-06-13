51882d17b241e2e0a8cfe2173a67b4bb33b847e5
Andres Mauricio Jimenez Chavez <142183331+MauricioJC3@users.noreply.github.com>
2025-06-13 08:54:15 -0500

    feat: commit bas

--- 02 - php/funciones.php
+++ 02 - php/funciones.php
@@ -7,7 +7,7 @@ echo "Marca de tiempo: " . time() . "<br>";
 // con sqrt se saca la raiz cuadrada de un numero
 echo "Raiz cuadrada de 9: " . sqrt(9) . "<br>";
 
-// Rand me sirve para poner un rando de un numero a otro yq ue aprezca aleatoria
+// Rand me sirve para poner un rando de un numero a otro y que aprezca aleatoria
 echo "Numero aleatorio entre 90 y 100 es: " . rand(90, 100) . "<br>";
 
 // Numero PI
